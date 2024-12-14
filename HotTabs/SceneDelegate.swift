7c2a103f5dd49645643a894c8d17db67377f7f9f
José Anchieta <santosjr87@gmail.com>
2024-12-14 12:13:38 -0300

    Add tab bars, this need to be fixed so we can see the proposal props

--- HotTabs/SceneDelegate.swift
+++ HotTabs/SceneDelegate.swift
@@ -5,6 +5,13 @@
 //  Created by José Anchieta on 13/12/24.
 //
 
+//
+//  SceneDelegate.swift
+//  HotTabs
+//
+//  Created by José Anchieta on 13/12/24.
+//
+
 import HotwireNative
 import UIKit
 
@@ -18,13 +25,34 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       .server(rootURL.appendingPathComponent("configurations/ios/v1.json"))
     ])
     
-    private lazy var navigator = Navigator(pathConfiguration: pathConfiguration, delegate: self)
-
+    private lazy var navigators: [Navigator] = {
+            return [
+                createNavigator(tag: 0),
+                createNavigator(tag: 1),
+                createNavigator(tag: 2)
+            ]
+        }()
+    
+    private lazy var tabBarController = TabBarController(navigators: navigators)
+    
+    private func createNavigator(tag: Int) -> Navigator {
+            let navigator = Navigator(pathConfiguration: pathConfiguration, delegate: self)
+            navigator.rootViewController.view.tag = tag
+            return navigator
+        }
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
         
-        window?.rootViewController = navigator.rootViewController
-        navigator.route(rootURL)
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+        
+        // Initial routing com delay para garantir que a UI esteja pronta
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    self.navigators[0].route(rootURL)
+                    self.navigators[1].route(rootURL.appendingPathComponent("todos"))
+                    self.navigators[2].route(rootURL.appendingPathComponent("pages/me"))
+                }
     }
 }
 
@@ -33,12 +61,45 @@ extension SceneDelegate: NavigatorDelegate {
         print("Proposal")
         print(proposal.properties)
         
-        let shouldHideNavbar = proposal.properties["hide_navbar"] as? Bool ?? false
-                
-        if let navigationController = navigator.rootViewController as? UINavigationController {
-            navigationController.setNavigationBarHidden(shouldHideNavbar, animated: true)
-        }
+//        let shouldHideNavbar = proposal.properties["hide_navbar"] as? Bool ?? false
+//
+//        if let navigationController = navigator.rootViewController as? UINavigationController {
+//            navigationController.setNavigationBarHidden(shouldHideNavbar, animated: true)
+//        }
         
         return .accept
+    }
+    
+    func willNavigate(visit: Visitable) {
+            print("Will Navigate:")
+        print("URL:", visit.visitableURL)
+        print("Action:", visit.visitableViewController)
+        }
+        
+    func didNavigate(visit: Visitable) {
+            print("Did Navigate:")
+            print("URL:", visit.visitableURL)
+        }
+    
+}
+
+
+class TabBarController: UITabBarController {
+    private let navigators: [Navigator]
+    
+    init(navigators: [Navigator]) {
+        self.navigators = navigators
+        super.init(nibName: nil, bundle: nil)
+        
+        viewControllers = navigators.map { $0.rootViewController }
+        
+        // Customize tab bar items
+        viewControllers?[0].tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 0)
+        viewControllers?[1].tabBarItem = UITabBarItem(title: "Posts", image: UIImage(systemName: "play.circle"), tag: 1)
+        viewControllers?[2].tabBarItem = UITabBarItem(title: "Playlists", image: UIImage(systemName: "list.number"), tag: 2)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
