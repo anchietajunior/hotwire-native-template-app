3bc02a097856e963b53e574bb00a692a000bd5c5
José Anchieta <santosjr87@gmail.com>
2024-12-16 16:04:32 -0300

    Refactoring

--- HotTabs/SceneDelegate.swift
+++ HotTabs/SceneDelegate.swift
@@ -8,24 +8,29 @@
 import HotwireNative
 import UIKit
 
-let rootURL = URL(string: "http://localhost:3000")!
+// MARK: - Constants
+private enum Constants {
+    static let rootURL = URL(string: "http://localhost:3000")!
+    
+    enum Tab {
+        static let items: [(title: String, icon: String, path: String)] = [
+            ("Home", "house", ""),
+            ("Posts", "list.number", "todos"),
+            ("Playlists", "person", "pages/me")
+        ]
+    }
+}
 
+// MARK: - SceneDelegate
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     private lazy var pathConfiguration = PathConfiguration(sources: [
-      .file(Bundle.main.url(forResource: "path-configuration", withExtension: "json")!),
-      .server(rootURL.appendingPathComponent("configurations/ios/v1.json"))
+        .file(Bundle.main.url(forResource: "path-configuration", withExtension: "json")!),
+        .server(Constants.rootURL.appendingPathComponent("configurations/ios/v1.json"))
     ])
     
-    private lazy var navigators: [Navigator] = {
-        return [
-            createNavigator(tag: 0),
-            createNavigator(tag: 1),
-            createNavigator(tag: 2)
-        ]
-    }()
-    
+    private lazy var navigators: [Navigator] = (0..<Constants.Tab.items.count).map { createNavigator(tag: $0) }
     private lazy var tabBarController = TabBarController(navigators: navigators)
     
     private func createNavigator(tag: Int) -> Navigator {
@@ -34,37 +39,46 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         return navigator
     }
     
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
-        
-        window = UIWindow(windowScene: scene as! UIWindowScene)
-        window?.rootViewController = tabBarController
-        window?.makeKeyAndVisible()
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-            self.navigators[0].route(rootURL)
-            self.navigators[2].route(rootURL.appendingPathComponent("pages/me"))
-            self.navigators[1].route(rootURL.appendingPathComponent("todos"))
-        }
-    }
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+          guard let windowScene = scene as? UIWindowScene else { return }
+          
+          window = UIWindow(windowScene: windowScene)
+          window?.rootViewController = tabBarController
+          window?.makeKeyAndVisible()
+          
+          DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+              self.navigators.forEach { navigator in
+                  if let navController = navigator.rootViewController as? UINavigationController {
+                      navController.setNavigationBarHidden(false, animated: false)
+                  }
+              }
+              
+              self.navigators.enumerated().forEach { index, navigator in
+                  let path = Constants.Tab.items[index].path
+                  navigator.route(Constants.rootURL.appendingPathComponent(path))
+              }
+          }
+      }
 }
 
+// MARK: - NavigatorDelegate
 extension SceneDelegate: NavigatorDelegate {
     func handle(proposal: VisitProposal) -> ProposalResult {
         print("🗨️ Proposal \(proposal)")
-            
-            let shouldHideNavbar = proposal.properties["hide_navbar"] as? Bool ?? false
-            
-            DispatchQueue.main.async {
-                if let navController = self.tabBarController.selectedViewController as? UINavigationController {
-                    navController.setNavigationBarHidden(shouldHideNavbar, animated: true)
-                }
+        
+        let shouldHideNavbar = proposal.properties["hide_navbar"] as? Bool ?? false
+        
+        DispatchQueue.main.async {
+            if let navController = self.tabBarController.selectedViewController as? UINavigationController {
+                navController.setNavigationBarHidden(shouldHideNavbar, animated: true)
             }
-            return .accept
         }
+        return .accept
+    }
 }
 
-class TabBarController: UITabBarController, UITabBarControllerDelegate {
+// MARK: - TabBarController
+class TabBarController: UITabBarController {
     private let navigators: [Navigator]
     
     init(navigators: [Navigator]) {
@@ -73,40 +87,35 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         
         viewControllers = navigators.map { $0.rootViewController }
         
-        viewControllers?[0].tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 0)
-        viewControllers?[1].tabBarItem = UITabBarItem(title: "Posts", image: UIImage(systemName: "play.circle"), tag: 1)
-        viewControllers?[2].tabBarItem = UITabBarItem(title: "Playlists", image: UIImage(systemName: "list.number"), tag: 2)
+        viewControllers?.enumerated().forEach { index, controller in
+            let item = Constants.Tab.items[index]
+            controller.tabBarItem = UITabBarItem(
+                title: item.title,
+                image: UIImage(systemName: item.icon),
+                tag: index
+            )
+        }
         
         delegate = self
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        viewControllers?.forEach { viewController in
-            if let navController = viewController as? UINavigationController {
-                navController.setNavigationBarHidden(false, animated: false)
-            }
-        }
-    }
-    
-    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
-        guard let index = viewControllers?.firstIndex(of: viewController) else { return }
-        
-        let navigator = navigators[index]
-        switch index {
-        case 0:
-            navigator.route(rootURL)
-        case 1:
-            navigator.route(rootURL.appendingPathComponent("todos"))
-        case 2:
-            navigator.route(rootURL.appendingPathComponent("pages/me"))
-        default:
-            break
-        }
-    }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewControllers?.forEach { viewController in
+            (viewController as? UINavigationController)?.setNavigationBarHidden(false, animated: false)
+        }
+    }
+}
+
+// MARK: - UITabBarControllerDelegate
+extension TabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        guard let index = viewControllers?.firstIndex(of: viewController) else { return }
+        let path = Constants.Tab.items[index].path
+        navigators[index].route(Constants.rootURL.appendingPathComponent(path))
     }
 }
